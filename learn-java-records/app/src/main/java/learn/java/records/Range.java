e96c0c26e5f46a4870cd30834525f80583c952f9
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2025-07-05 15:21:02 +0700

    refactor: update Range record to use compact constructor with validation

--- learn-java-records/app/src/main/java/learn/java/records/Range.java
+++ learn-java-records/app/src/main/java/learn/java/records/Range.java
@@ -2,12 +2,20 @@ package learn.java.records;
 
 public record Range(int start, int end) {
     // Customize Canonical Constructors
-    public Range(int start, int end) {
+    // public Range(int start, int end) {
+    // // add some validations
+    // if (start >= end)
+    // throw new Error("Invalid input, the start field need to be smaller than the
+    // end field.");
+
+    // this.start = start;
+    // this.end = end;
+    // }
+
+    // Customize Compact Constructors
+    public Range {
         // add some validations
         if (start >= end)
             throw new Error("Invalid input, the start field need to be smaller than the end field.");
-
-        this.start = start;
-        this.end = end;
     }
 }
